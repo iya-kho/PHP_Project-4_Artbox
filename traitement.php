a370b2aa92eeedbccefcd29444cbe5b15c2e3b31
Iya Shchur <iya.schur@gmail.com>
2024-01-09 11:40:40 +0200

    add to database

--- traitement.php
+++ traitement.php
@@ -4,14 +4,14 @@ require 'header.php';
 
 $formData = $_POST;
 
-// Vérifiez que le formulaire a été soumis
+// Vérifier que le formulaire a été soumis
 
 if (!isset($formData['submit'])) {
   header('Location: index.php');
   exit();
 }
 
-// Vérifiez que tous les champs sont remplis
+// Vérifier que tous les champs sont remplis
 
 function isEmpty($data) {
   foreach ($data as $key => $value) {
@@ -22,7 +22,7 @@ function isEmpty($data) {
   return false;
 }
 
-//Vérifiez que la forme est valide
+//Vérifier que la forme est valide
 
 if (isEmpty($formData)) {
   echo 'Veuillez remplir tous les champs';
@@ -39,6 +39,22 @@ $titre = htmlspecialchars($formData['titre']);
 $artiste = htmlspecialchars($formData['artiste']);
 $image = htmlspecialchars($formData['image']);
 $description = htmlspecialchars($formData['description']);
+
+// Ajouter l'oeuvre à la base de données
+
+require 'bdd.php';
+
+$bdd = connexionBdd();
+$sqlQuery = 'INSERT INTO artworks (title, artist, description, imageUrl) VALUES (:titre, :artiste, :description, :image)';
+$req = $bdd->prepare($sqlQuery);
+$req->execute([
+  'titre' => $titre,
+  'artiste' => $artiste,
+  'description' => $description,
+  'image' => $image
+]);
+
+header('Location: oeuvre.php?id=' . $bdd->lastInsertId());
 
 require 'footer.php';
 
